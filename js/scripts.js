dc9979c7167f72faca40e1982546293456e8637a
Alexis Carando <carse88@hotmail.com>
2022-08-25 05:40:58 -0300

    Javascrip

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let n = Number(prompt("EL siguiente numero sera sumado con valores que van del 1 al 10"));
+for (i = 1; i <= 10 ; i++){
+    let resultado = n + i;
+    console.log(`${n}+${i}=${resultado}`)
+}
+console.log(`Fin de la suma`);
