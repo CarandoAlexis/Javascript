2761788170aeadaf055ec05639701c007436f561
Alexis Carando <carse88@hotmail.com>
2022-08-29 22:24:04 -0300

    Agregado mensaje de error ingresando numero mayor a 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ let nota2 = 0;
 let nota3 = 0;
 let nota4 = 0;
 let nota5 = 0;
-let numeronotas = Number(prompt("ingrese numero de notas a promediar no mayor a 5"))
+let numeronotas = Number(prompt("Ingrese un numero de notas a promediar no mayor a 5"))
 function promedio(numeronotas){
   if (numeronotas <= 1){
-    return "el valor debe ser mayor a 1";
+    return "La cantidad de notas debe ser mayor a 1";
+  }else if (numeronotas >= 6){
+    return "La cantidad de notas debe ser menor a 6";
   }else if(numeronotas == 2){
     let nota1 = Number(prompt("ingrese nota 1"))
     let nota2 = Number(prompt("ingrese nota 2"))
@@ -32,7 +34,7 @@ function promedio(numeronotas){
   }
 }
 
-  console.log(promedio(numeronotas))
+console.log(promedio(numeronotas))
 
 
 
