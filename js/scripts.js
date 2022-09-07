d34426decdab6953f9f7808343eaf6a0051f0edf
Alexis Carando <carse88@hotmail.com>
2022-09-07 13:43:28 -0300

    Array de alumnos con pequeños cambios en ciclo for

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,10 @@
-let alumnos = Number(prompt("ingrese numero de alumnos")) ;
+let alumnos = ["Juan", "Adrian", "Ana" , "Martin"];
 let nota1 = 0;
 let nota2 = 0;
 let nota3 = 0;
 let nota4 = 0;
 let nota5 = 0;
 let numeronotas = Number(prompt("Ingrese un numero de notas a promediar no mayor a 5"));
-
-function nombrealumno(){
-  let nombre = prompt("Ingrese nombre");
-  let apellido = prompt("Ingrese apellido");
-  return nombre+" "+apellido;
-}
-
 function promedio(numeronotas){
 
   if (numeronotas <= 1){
@@ -21,34 +14,37 @@ function promedio(numeronotas){
   }else if(numeronotas == 2){
     let nota1 = Number(prompt("ingrese nota 1"))
     let nota2 = Number(prompt("ingrese nota 2"))
-        return (nota1 + nota2)/2;
+        return (nota1 + nota2)/numeronotas;
   }else if(numeronotas == 3){
     let nota1 = Number(prompt("ingrese nota 1"))
     let nota2 = Number(prompt("ingrese nota 2"))
     let nota3 = Number(prompt("ingrese nota 3"))
-        return (nota1 + nota2 +nota3)/3;
+        return (nota1 + nota2 +nota3)/numeronotas;
   }else if(numeronotas == 4){
     let nota1 = Number(prompt("ingrese nota 1"))
     let nota2 = Number(prompt("ingrese nota 2"))
     let nota3 = Number(prompt("ingrese nota 3"))
     let nota4 = Number(prompt("ingrese nota 4"))
-        return (nota1 + nota2 + nota3 + nota4)/4;
+        return (nota1 + nota2 + nota3 + nota4)/numeronotas;
   }else (numeronotas == 5);{
     let nota1 = Number(prompt("ingrese nota 1"))
     let nota2 = Number(prompt("ingrese nota 2"))
     let nota3 = Number(prompt("ingrese nota 3"))
     let nota4 = Number(prompt("ingrese nota 4"))
     let nota5 = Number(prompt("ingrese nota 5"))
-        return (nota1 + nota2 + nota3 + nota4 + nota5)/5;
+        return (nota1 + nota2 + nota3 + nota4 + nota5)/numeronotas;
   
   }
 }
-
-for (let i = 0; i < alumnos; i++) {
-  
-  console.log(nombrealumno()+" Promedio: "+promedio(numeronotas));
-
+for (let i = 0; i < 4; i++) {
+  if (numeronotas <= 1) {
+    console.log(promedio(numeronotas))
+    break;
+  } else if(numeronotas >= 6){
+    console.log(promedio(numeronotas))
+    break;
+  } else{
+    alert("Ingrese las notas de "+alumnos[i])
+    console.log(alumnos[i]+" Promedio: "+promedio(numeronotas));
+  }
 }
-
-
-
